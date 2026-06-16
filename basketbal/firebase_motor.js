3ebdadeb0c6c44696b1ebf8eccf1084aab61523e
tomboms <47323925+tomboms@users.noreply.github.com>
2026-06-16 12:20:27 +0200

    1

--- basketbal/firebase_motor.js
+++ basketbal/firebase_motor.js
@@ -1,6 +1,6 @@
 // --- FIREBASE_MOTOR.JS: LIVE SYNC & KOGELVRIJE OPSLAG ---
 
-const APP_VERSIE = "Beta v5.4B";
+const APP_VERSIE = "Beta v5.4C";
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 import { getFirestore, doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
